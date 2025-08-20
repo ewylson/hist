fca0d27a371861a7cbc7646abd8d6d5eb675aef7
Ewilson <52111043+Ev1ls0n@users.noreply.github.com>
2025-08-20 22:03:42 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	cc main.c -o hist -Wall -Wextra -pedantic -std=c99 -O2
---
+++ main.c
@@ -0,0 +1,77 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASCII_SIZE 256
+#define BUFFER_SIZE 512
+
+#define HISTOGRAM_SYMBOL_FILL '#'
+#define HISTOGRAM_SYMBOL_BLANK '_'
+
+struct SymbolAmount {
+    unsigned char symbol;
+    size_t amount;
+} ascii_symbols[ASCII_SIZE];
+
+int compare_symbols(const void *a, const void *b)
+{
+    size_t amount_a = ((struct SymbolAmount*)a)->amount;
+    size_t amount_b = ((struct SymbolAmount*)b)->amount;
+    return amount_b - amount_a;
+}
+
+void parse_symbols(const unsigned char *symbols)
+{
+    while (*symbols) {
+        if (!iscntrl(*symbols)) {
+            ascii_symbols[*symbols].symbol = *symbols;
+            ascii_symbols[*symbols].amount++;
+        }
+        symbols++;
+    }
+    return;
+}
+
+void draw_bar(const unsigned char symbol, const size_t range, const size_t range_max)
+{
+    char bar[range_max + 1];
+    for (size_t i = 0; i < range_max; i++)
+        bar[i] = (i < range) ? HISTOGRAM_SYMBOL_FILL : HISTOGRAM_SYMBOL_BLANK;
+    printf("%c: [%s]\n", symbol, bar);
+    return;
+}
+
+void build_histogram(const size_t histogram_range_max)
+{
+    qsort(ascii_symbols, ASCII_SIZE, sizeof(struct SymbolAmount), compare_symbols);
+
+    const size_t max_value = ascii_symbols[0].amount;
+    for (unsigned char i = 0; ascii_symbols[i].amount > 0; i++) {
+        const float scale = (float)ascii_symbols[i].amount / max_value;
+        draw_bar(ascii_symbols[i].symbol, scale * histogram_range_max, histogram_range_max);
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        goto func_end;
+
+    FILE *ptr_file = fopen(argv[argc - 1], "r");
+
+    if (!ptr_file) {
+        printf("(!) Not able to open the file.\n");
+        goto func_end;
+    }
+
+    unsigned char buffer[BUFFER_SIZE];
+    while (fgets((char*)buffer, BUFFER_SIZE, ptr_file))
+        parse_symbols(buffer);
+    fclose(ptr_file);
+
+    build_histogram(16);
+
+func_end:
+    return EXIT_SUCCESS;
+}
